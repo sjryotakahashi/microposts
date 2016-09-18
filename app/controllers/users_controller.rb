7d418b54c6625f4413fd97108091eaaaed702edd
ryotakahashi <sjrt33m@ezweb.ne.jp>
2016-09-18 12:57:43 +0000

    user-profile-1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
+  before_action :correct_user, only: [:edit, :update]
 
-  def show # 追加
+  def show
    @user = User.find(params[:id])
   end
   
@@ -36,6 +37,10 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation, :location)
+  end
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_path if @user != current_user
   end
 end
