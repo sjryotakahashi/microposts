f29fd404f9e71f9b580d75c5a39bf7cd8647a36e
ryotakahashi <sjrt33m@ezweb.ne.jp>
2016-09-23 07:24:21 +0000

    followings-followers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
+  before_action :set_params, only: [:show, :edit, :update, :followings, :followers]
   before_action :correct_user, only: [:edit, :update]
 
   def show
-   @user = User.find(params[:id])
     @microposts = @user.microposts.order(created_at: :desc)
   end
   
@@ -21,17 +21,27 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Updated Profile!"
       redirect_to @user
     else
       render 'edit'
     end
+  end
+  
+  def followings
+    @title = 'Followings'
+    @users = @user.following_users
+    render 'show_follow'
+  end
+  
+  def followers
+    @title = 'Followers'
+    @users = @user.follower_users
+    render 'show_follow'
   end
 
   private
@@ -40,8 +50,12 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation, :location)
   end
-  def correct_user
+
+  def set_params
     @user = User.find(params[:id])
+  end
+  
+  def correct_user
     redirect_to root_path if @user != current_user
   end
 end
